0afaa01fdfeb852d7b5fb50c092ed2c4074e3056
jibeom <jibeom.0408@gmail.com>
2023-04-27 12:38:20 +0900

    서버 수정

--- server/app.js
+++ server/app.js
@@ -1,20 +1,40 @@
+const riot_api_key = 'RGAPI-be10431b-61b7-4dd2-8119-f89938c68597'
+
 const port = 80;
+
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const request = require('sync-request');
+const urlencode = require('urlencode');
 const morgan = require('morgan');
 
 const app = express();
 
-// app.use(morgan('dev'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
+// app.use('/', express.static(path.join(__dirname, '../client/html/main.html')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/html/main.html'));
 });
 
 app.post('/search', (req, res) => {
-  // let username = req.body.username;
-  res.send(req.body);
+  const username = req.body.username;
+  var puuid;
+  const find_PUUID_url = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + username + '?api_key=' + riot_api_key;
+  request(find_PUUID_url, (err, res, body) => {
+    const info_json = JSON.parse(body);
+    // const key = Object.keys(info_json);
+    // puuid = info_json[key]["id"];
+    puuid = info_json.puuid;
+    console.log(puuid);
+  });
+  res.send(puuid);
+  console.log(puuid);
 });
 
 // 404 미들웨어 ----------------------------------------------------------------------------------
